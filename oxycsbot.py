a3e8d1c78c27396fd8830e5e236bfab45c3af600
logzee <45110509+logzee@users.noreply.github.com>
2020-03-21 15:55:33 -0500

    Update oxycsbot.py

--- oxycsbot.py
+++ oxycsbot.py
@@ -238,92 +238,89 @@ class OxyCSBot(ChatBot):
             return self.go_to_state('didnt_catch_the_last_game')
         return self.finish('confused')
 	
-                       
-        #response if they officially don't like the lakers but like another team
-        def on_enter_lakers_hater(self):
-            return " ".join([ "You're a", self.team.capitalize(), "fan?!",
-                             "Im really trying to be nicer on the ol' web, after those punk ass mods banned me from r/NBA, but c'mon your a",
-                             self.team, "fan!", "I honestly thought they got relegated to the B league after their last season"])
-        
-        # the bot exits in an angry fashion
-        def respond_from_lakers_hater(self):
-            return self.finish('angry')
-        
-        # theyve seen the last game of the season
-        def on_enter_saw_last_game(self):
-            if self.gamestatnum = 0:
-                return "Obviously disapointed about the outcome, but even in defeat the lakers where a pleasure to watch. It sucks we wont see them in action for a while :( ya know with everything thats going on..."
-            elif self.gamestatnum = 1:
-                return "I still can't believe it, I don't even know what to do with myself these days. No school, no sports, its all so strange"
-            elif self.gamestatnum = 2:
-                return "Never thought i'd see that day that an entire season was canceled, this virus is some really crazy shit."
-            elif self.gamestatnum = 3:
-                return "A whole season shutdown because of a virus, like I get it but still. Its crazy"
-            elif self.gamestatnum = 4:
-                return "Have you not heard? The NBA has ended its season untill this whole coronavirus things gets sorted out"   
-            return self.go_to_state('corona_virus')
-        
-        #take response and redirect
-        def respond_from_saw_last_game(self, message, tags):
-            if 'coronavirus' in tags:
-                if 'canceled' in tags:
-                    self.gamestatnum += 1
-                    return self.go_to_state('saw_last_game')
-                else:
-                    self.gamestatnum += 2
-                    return self.go_to_state('saw_last_game')
-                elif 'canceled' in tags:
-                    self.gamestatnum += 3
-                    return self.go_to_state('saw_last_game')
-                elif 'no' in tags:
-                    self.gamestatnum += 4
-                    return self.go_to_state('saw_last_game')
-                return self.finish('confused')
-            return self.finish('confused')
-        
-        #didnt see the last game
-        def on_enter_didnt_catch_last_game(self):
-            return "Well it sucks you missed what might be the last game of the season :/"
-        
-        def respond_from_didnt_catch_last_game(self, message, tags):
-            if 'coronavirus' in tags:
-                return self.go_to_state('corona_virus')
-            return self.finish('confused')
-        
-        def on_enter_corona_virus(self):
-            return "My cities gone full shutdown because of coronavirus, what are you doing since everythings shutdown?"
-        
-        def respond_from_corona_virus(self, message, tags):
-            if 'school' in tags:
-                if 'canceled' in tags:
-                    return self.finish('home')   
-                elif 'work' in tags:
-                    if 'canceled' in tags:
-                        return self.finish('home')
-                    elif 'sick' in tags:
-                        return self.finish('sick')
-                    return self.finish('confused')
-                return self.finish('confused')
-            return self.finish('confused')
-        
-        #finish states
-        def finish_sick(self):
-            return "I'm sorry to hear that, well I hope it doesn't get to bad for you. I gotta get back to work, even from home managment is breathing down my neck, but take it easy. Hope you feel better soon"
-        
-		def finish_home(self):   
-            return "I'm stuck at home too and theres not even basketball to help pass the time. Ugh, I should actually head out. They've got me working from home, and another task just game in via slack :("
-        
-        #confused
-        def finish_confused(self):
-            return "I gotta be honest, I'm not the brightest bulb in the drawer. I didn't undestand a word you just said"
-        
-        # responds with angry exit message, moves back to waiting state
-        def finish_angry(self):
-            return "Honestly can't even process what your saying right now. Not sure if its the 6 pack of coors im sipping on, or the concusion I got last week while playing at the rec center but im furious. I need to go cool off, I hope when Im back youve come to ur senses and support the lakers"
-        
-        #exit not a basketball fan
-        def finish_noBasketball(self):
-            return "It doesn't seem like you're much of a basketball fan... Thats really the only thing I know how to talk about :/ Hit me up if u get an interest in the sport, till im gonna blast."	
+	#response if they officially don't like the lakers but like another team
+	def on_enter_lakers_hater(self):
+		return_msg =  " ".join([ "You're a", self.team.capitalize(), "fan?!",
+								"Im really trying to be nicer on the ol' web, after those punk ass mods banned me from r/NBA, but c'mon your a",
+								self.team, "fan!", "I honestly thought they got relegated to the B league after their last season"])
+        return return_msg
+	# the bot exits in an angry fashion
+	def respond_from_lakers_hater(self):
+		return self.finish('angry')
+	
+	# theyve seen the last game of the season
+	def on_enter_saw_last_game(self):
+		if self.gamestatnum = 0:
+			return "Obviously disapointed about the outcome, but even in defeat the lakers where a pleasure to watch. It sucks we wont see them in action for a while :( ya know with everything thats going on..."
+		elif self.gamestatnum = 1:
+			return "I still can't believe it, I don't even know what to do with myself these days. No school, no sports, its all so strange"
+		elif self.gamestatnum = 2:
+			return "Never thought i'd see that day that an entire season was canceled, this virus is some really crazy shit."
+		elif self.gamestatnum = 3:
+			return "A whole season shutdown because of a virus, like I get it but still. Its crazy"
+		elif self.gamestatnum = 4:
+			return "Have you not heard? The NBA has ended its season untill this whole coronavirus things gets sorted out"   
+		return self.go_to_state('corona_virus')
+	#take response and redirect
+	def respond_from_saw_last_game(self, message, tags):
+		if 'coronavirus' in tags:
+			if 'canceled' in tags:
+				self.gamestatnum += 1
+				return self.go_to_state('saw_last_game')
+			else:
+				self.gamestatnum += 2
+				return self.go_to_state('saw_last_game')
+			elif 'canceled' in tags:
+				self.gamestatnum += 3
+				return self.go_to_state('saw_last_game')
+			elif 'no' in tags:
+				self.gamestatnum += 4
+				return self.go_to_state('saw_last_game')
+			return self.finish('confused')
+		return self.finish('confused')
+	#didnt see the last game
+	def on_enter_didnt_catch_last_game(self):
+		return "Well it sucks you missed what might be the last game of the season :/"
+	
+	def respond_from_didnt_catch_last_game(self, message, tags):
+		if 'coronavirus' in tags:
+			return self.go_to_state('corona_virus')
+		return self.finish('confused')
+	
+	def on_enter_corona_virus(self):
+		return "My cities gone full shutdown because of coronavirus, what are you doing since everythings shutdown?"
+	
+	def respond_from_corona_virus(self, message, tags):
+		if 'school' in tags:
+			if 'canceled' in tags:
+				return self.finish('home')   
+			elif 'work' in tags:
+				if 'canceled' in tags:
+					return self.finish('home')
+				elif 'sick' in tags:
+					return self.finish('sick')
+				return self.finish('confused')
+			return self.finish('confused')
+		return self.finish('confused')
+	
+	#finish states
+	def finish_sick(self):
+		return "I'm sorry to hear that, well I hope it doesn't get to bad for you. I gotta get back to work, even from home managment is breathing down my neck, but take it easy. Hope you feel better soon"
+	
+	def finish_home(self):   
+		return "I'm stuck at home too and theres not even basketball to help pass the time. Ugh, I should actually head out. They've got me working from home, and another task just game in via slack :("
+	
+	#confused
+	def finish_confused(self):
+		return "I gotta be honest, I'm not the brightest bulb in the drawer. I didn't undestand a word you just said"
+	
+	# responds with angry exit message, moves back to waiting state
+	def finish_angry(self):
+		return "Honestly can't even process what your saying right now. Not sure if its the 6 pack of coors im sipping on, or the concusion I got last week while playing at the rec center but im furious. I need to go cool off, I hope when Im back youve come to ur senses and support the lakers"
+	
+	#exit not a basketball fan
+	def finish_noBasketball(self):
+		return "It doesn't seem like you're much of a basketball fan... Thats really the only thing I know how to talk about :/ Hit me up if u get an interest in the sport, till im gonna blast."	
         
 if __name__ = '__main__':
     OxyCSBot().chat()
